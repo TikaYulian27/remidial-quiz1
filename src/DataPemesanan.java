76ffb150c0b609c4fd6db72af7746d1f2ecb51b5
Tika Yulianti <tikayulian27@gmail.com>
2018-11-04 13:00:50 +0700

    membuat class yang dibutuhkan terlebih dahulu

--- src/DataPemesanan.java
+++ src/DataPemesanan.java
@@ -8,7 +8,6 @@
  *
  * @author Tika Yln
  */
-public class JahitPakaian {
+public class DataPemesanan {
     
-    // test java
 }
---
+++ src/Jahitan.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Tika Yln
+ */
+public class Jahitan {
+    
+    protected String jenisPesanan;
+    protected String size;
+
+    public void setJenisPesanan(String jenisPesanan) {
+        this.jenisPesanan = jenisPesanan;
+    }
+    
+    public String getJenisPesanan() {
+        return jenisPesanan;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+    
+    public String getSize() {
+        return size;
+    }  
+}
