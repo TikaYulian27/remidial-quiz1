96d7a641956a8c27db64998fa71f4627f587e1a4
Tika Yulianti <tikayulian27@gmail.com>
2018-11-04 13:41:10 +0700

    membuat main dan mencoba runing

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Tika Yln
+ */
+public class Main {
+    
+    public static void main(String[] args){
+        
+        Jahitan pesanJahitan = new Jahitan();
+        DataPemesanan anggota = new DataPemesanan();
+        
+        anggota.setNama("Tika Yulianti Cantik Sekali");
+        anggota.setAlamat("Tulungagung");
+        
+        pesanJahitan.setSize("M");
+        pesanJahitan.setJenisPesanan("Gaun Pesta");
+        
+        Pemesanan pesan = new Pemesanan();
+        pesan.pesan(pesanJahitan, anggota);
+        pesan.infoDataPemesan();
+    }
+}
